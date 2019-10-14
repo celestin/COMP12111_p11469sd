5ab78f49903c4907a51fe1119bd82f4aed3b9d77
Sam Da Costa <samuel.dacosta-2@student.manchester.ac.uk>
2019-10-14 18:02:43 +0100

    7 Seg done

--- COMP12111_lib/sevensegmentdecoder/functional/verilog.v
+++ COMP12111_lib/sevensegmentdecoder/functional/verilog.v
@@ -10,11 +10,27 @@ module sevensegmentdecoder (input [3:0] bcd,
 // create an always block to represent the desired behaviour of the
 // combinatorial design
 // -----------------------------------------------------------------
+always @ (bcd)
+  case (bcd)
+    4'b0000: segments = 8'b00111111;//0
+    4'b0001: segments = 8'b00000110;//1
+    4'b0010: segments = 8'b01011011;//2
+    4'b0011: segments = 8'b01001111;//3
 
+    4'b0100: segments = 8'b01100110;//4
+    4'b0101: segments = 8'b01101101;//5
+    4'b0110: segments = 8'b01111101;//6
+    4'b0111: segments = 8'b00000111;//7
 
+    4'b1000: segments = 8'b01111111;//8
+    4'b1001: segments = 8'b01101111;//9
+    4'b1010: segments = 8'b01110111;//A
+    4'b1011: segments = 8'b01111100;//B
 
-
-
-
+    4'b1100: segments = 8'b00111001;//C
+    4'b1101: segments = 8'b01011110;//D
+    4'b1110: segments = 8'b01111001;//E
+    4'b1111: segments = 8'b01110001;//F
+  endcase
 
 endmodule  // end of module sevensegmentdecoder
--- sevensegmentdecoder_run1/stimulus.v
+++ sevensegmentdecoder_run1/stimulus.v
@@ -16,7 +16,21 @@ begin
 // Make sure you test all input combinations with a delay
 // -------------------------------------------------------
 
-
+#100 bcd=1;
+#100 bcd=2;
+#100 bcd=3;
+#100 bcd=4;
+#100 bcd=5;
+#100 bcd=6;
+#100 bcd=7;
+#100 bcd=8;
+#100 bcd=9;
+#100 bcd=10;
+#100 bcd=11;
+#100 bcd=12;
+#100 bcd=13;
+#100 bcd=14;
+#100 bcd=15;
 
 
 
